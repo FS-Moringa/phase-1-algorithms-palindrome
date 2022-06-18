89f09628f7c5ee50ea3fbaea6abd0b92363ec1c6
stensis <irenenjuguna98@gmail.com>
2022-06-18 11:03:58 +0300

    algorithms palindrome

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let arr = word.split("")
+  let reversedArr = arr.reverse()
+  let reversedWord = reversedArr.join("")
+  if (word == reversedWord){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
 */
-
+// Turn a string (word) into an Array (arr) - .split("")
+//   Reverse the elements in the Array - .reverse()
+//   Turn the Array into a string - .join("")
+//   Use if statement to test if word and reversedWord are ==
+//     if true, return true
+//     if false, return false
 /*
   Add written explanation of your solution here
 */
